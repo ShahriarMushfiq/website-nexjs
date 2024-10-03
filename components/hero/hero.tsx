6c323919b210ff2088e536a6d016592e90070684
Nobir <25162441+nobir@users.noreply.github.com>
2024-10-03 15:27:47 +0600

    [Update] Responsive the `Hero` component

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -10,13 +10,14 @@ export const Hero = ({ title, description, children, ...rest }: HeroProps) => {
     <Flex py="20" alignItems="center" {...rest}>
       <Container>
         <VStack spacing={[4, null, 8]} alignItems="flex-start">
-          <Text as="h1" textStyle="h1" textAlign="left">
+          <Text as="h1" textStyle="h1" textAlign={{ base: "center", lg: "left" }}>
             {title}
           </Text>
           <Text
-            as="div"
+            as="span"
             textStyle="subtitle"
-            align="left"
+            width="100%"
+            textAlign={{ base: "center", lg: "left" }}
             color="gray.500"
             _dark={{ color: 'gray.400' }}
           >
